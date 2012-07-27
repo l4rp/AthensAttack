db22fda66d0f33a8ed2d941fb93c53005d73e297
aaron <aaron@crunchbang.(none)>
2012-07-27 15:04:16 +0100

    Add sms route for tropo

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/sms', routes.sms);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
