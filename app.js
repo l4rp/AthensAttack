525b075c19905b60ebb4593f179c59acf713ff73
aaron <aaron@crunchbang.(none)>
2012-07-27 17:11:38 +0100

    Fix syntax error

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.get('/sms', routes.sms);
-app.get('/attacks'. routes.attacks);
+app.get('/attacks', routes.attacks);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
